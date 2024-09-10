d376dead81466d2f07c75a0ec2c70036e4553dd2
tofu <floofy-tofu@protonmail.com>
2024-09-10 02:33:58 -0400

    Useful function

--- lisp/tofu-useful-functions.el
+++ lisp/tofu-useful-functions.el
@@ -7,7 +7,7 @@
 
 ;;; Scripting functions
 (defun tofu/path-in-user-emacs (file-path)
-  "Make a path relative to `user-emacs-directory'"
+  "Prepend `user-emacs-directory' to FILE-PATH."
   (concat user-emacs-directory file-path))
 
 
@@ -21,18 +21,33 @@
     (kill-new file-path)))
 
 (defun tofu/get-absolute-buffer-path-of (buffer)
-  "Prompt for buffer, show its full file path and add it to kill ring."
+  "Prompt for BUFFER, show its full file path and add it to kill ring."
   (interactive "b")
   (let ((file-path (buffer-file-name (get-buffer buffer))))
     (message file-path)
     (kill-new file-path)))
 
 (defun tofu/get-breakpoint-at-point ()
+  "Show get a /file/path:linenum style breakpoint marker and append to kill ring."
   (interactive)
   (let* ((file-path (buffer-file-name))
          (breakpoint (concat file-path ":" (int-to-string (line-number-at-pos)))))
     (message breakpoint)
     (kill-new breakpoint)))
+
+(defun tofu/insert-elisp-header-and-footer (description)
+  "Insert a header (with a prompted DESCRIPTION) and footer to make flymake happy.
+DOES NOT TRY TO BE SMART ABOUT IT AND CONSIDER IF IT ALREADY EXISTS OR FILE VARIABLES."
+  (interactive "sFile Description: ")
+  (let ((file (car (last (string-split buffer-file-name "/")))))
+    ;; There's gotta be a better way to extract the filename here...
+    (save-excursion
+      (goto-char (point-min))
+      (insert ";;; " file "---" description "-*- lexical-binding: t; -*-\n")
+      (insert ";;; Commentary:\n\n")
+      (insert ";;; Code:\n\n")
+      (goto-char (point-max))
+      (insert ";;;" file "ends here"))))
 
 
 (provide 'tofu-useful-functions)
