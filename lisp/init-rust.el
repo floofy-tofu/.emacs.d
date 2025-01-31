cf1eca55f03d3f6eec58f5d7426386949be66e39
tofu <floofy-tofu@protonmail.com>
2025-01-31 15:25:03 -0500

    Rust

---
+++ lisp/init-rust.el
@@ -0,0 +1,10 @@
+;;; init-rust.el --- Initialize Rust -*- lexical-binding: t; -*-
+;;; Commentary:
+
+;;; Code:
+
+(use-package rust-mode
+  :init
+  (setq rust-mode-treesitter-derive t))
+
+;;; init-rust.el ends here
